f279e1ec0548e00482b985f48b8f8602d586ce75
Nicolas <nicolas.7l@hotmail.com>
2020-06-05 08:46:33 -0300

    Initial commit

---
+++ server/src/database/connection.ts
@@ -0,0 +1,14 @@
+import knex from 'knex'
+import path from 'path'
+
+const connection = knex({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'numsey',
+    database : 'nextweek'
+  }
+})
+
+export default connection
